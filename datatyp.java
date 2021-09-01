237cdd14173c17383d36964c1db39236fa4403fe
snehaaraj27 <66328541+snehaaraj27@users.noreply.github.com>
2021-09-01 20:48:57 +0530

    Add files via upload

---
+++ datatyp.java
@@ -0,0 +1,15 @@
+public class Datatypes{
+  public static void main (string args[]){
+  int a=2;
+  float b=2.300;
+  double c=2564;
+  char d='W';
+  string e = "fish";
+   
+   system.out.println("int  "+a);
+   system.out.println("float "+b);
+   system.out.println("double"+c);
+   system.out.println("char"+d);
+   system.out.println("string"+e);
+}
+} 
